0856cda4a952ee56ecf7d2c64be7df08a9c271e0
SieurFenouil <cedryc.fenot@gmail.com>
2025-08-16 09:37:24 +0000

    New file/enums organisation, and new enum list for commands

--- app_code/game_state.py
+++ app_code/game_state.py
@@ -1,10 +1,6 @@
-from enum import Enum
+from ._common_enums.common_enums import GameStateEnum
+from ._common_enums.common_enums import PlayerCommands
 from .arena.arena import ArenaEnv
-
-class GameStateEnum(Enum):
-    SETUP = 0           # barracks stuff ? team creation, level ups, all that
-    BATTLE = 1          # this is a battle
-    BATTLE_OVER = 2     # this is a battle finished
 
 class GameState():
   def __init__(self):
@@ -33,5 +29,5 @@ class GameState():
               self.state = GameStateEnum.BATTLE_OVER
 
           case GameStateEnum.BATTLE_OVER:
-            if(command == 0x0001):
+            if(command == PlayerCommands.BACK_TO_SETUP):
               self.state = GameStateEnum.SETUP
